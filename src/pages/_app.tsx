4c6d778e6106ce78caf01f89fd788910dd9a6272
Mohith-Kumar28 <mohithkumar808@gmail.com>
2024-05-30 12:39:54 +0530

    updated google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
-import GoogleAnalytics from "@/components/util/providers/GoogleAnalytics";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GoogleAnalytics />
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || ""} />
       <Component {...pageProps} />
     </>
   );
