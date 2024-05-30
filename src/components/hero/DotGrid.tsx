c4e61739d50d3cea3624dcb0dee3c89c8278e4a3
Mohith-Kumar28 <mohithkumar808@gmail.com>
2024-05-30 11:44:17 +0530

    hero auto animation

--- src/components/hero/DotGrid.tsx
+++ src/components/hero/DotGrid.tsx
@@ -1,9 +1,51 @@
 import anime from "animejs";
+import { useEffect, useState } from "react";
 
 const GRID_WIDTH = 25;
 const GRID_HEIGHT = 20;
 
 const DotGrid = () => {
+  const [simulationActive, setSimulationActive] = useState(false);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    // Start the simulation when the component mounts
+    startSimulation();
+    return () => {
+      // Clear the interval when the component unmounts
+      clearInterval(intervalId as unknown as number);
+    };
+  }, []);
+
+  const startSimulation = () => {
+    if (!simulationActive) {
+      setSimulationActive(true);
+      const id = setInterval(() => {
+        // Generate random indices within the grid dimensions
+        const x = Math.floor(Math.random() * GRID_WIDTH);
+        const y = Math.floor(Math.random() * GRID_HEIGHT);
+
+        // Calculate the index based on the generated coordinates
+        const index = x + y * GRID_WIDTH;
+
+        // Trigger the click event on the randomly selected dot
+        const target = document.querySelector(`[data-index="${index}"]`);
+        if (target instanceof HTMLElement) {
+          target.click(); // Simulate click
+        }
+
+        // Stop the simulation after a certain condition is met
+        // For example, after simulating 100 clicks
+        if (Math.random() > 0.98) {
+          clearInterval(id);
+          setSimulationActive(false);
+        }
+      }, 3000); // Adjust the interval time as needed
+
+      setIntervalId(id);
+    }
+  };
+
   const handleDotClick = (e: any) => {
     anime({
       targets: ".dot-point",
