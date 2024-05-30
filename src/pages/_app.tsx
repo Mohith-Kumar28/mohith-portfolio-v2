b31f7943d655f199bcebcb1e2d4594c3c559ea1b
Mohith-Kumar28 <mohithkumar808@gmail.com>
2024-05-30 12:42:14 +0530

    empty commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || ""} />
+
       <Component {...pageProps} />
     </>
   );
