a65c070b37272c24b87d3874884d1fcff8414a09
Mohith-Kumar28 <mohithkumar808@gmail.com>
2024-06-03 22:52:16 +0530

    updated resume link to bitly

--- src/components/nav/Header.tsx
+++ src/components/nav/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
   return (
     <header className="h-[72px] px-4 flex items-center justify-between sticky top-0 z-20 bg-zinc-900/50 backdrop-blur-md">
       <MyLinks />
-      <OutlineButton onClick={() => window.open("/resume_v4.pdf")}>
+      <OutlineButton onClick={() => window.open("https://bit.ly/45cq8Oy")}>
         My Resume
       </OutlineButton>
     </header>
