810e550cfe6b4b0a9a743868b10070ce9901633d
Mohith-Kumar28 <mohithkumar808@gmail.com>
2024-08-03 02:18:01 +0530

    updated analytics import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
