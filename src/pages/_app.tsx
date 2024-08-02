d56e7281e186083148ead21afe1f8ef6110ab7d3
Mohith-Kumar28 <mohithkumar808@gmail.com>
2024-08-03 02:17:34 +0530

    added vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Analytics />
       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || ""} />
       {console.log("gtag", process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER)}
       <GoogleTagManager
