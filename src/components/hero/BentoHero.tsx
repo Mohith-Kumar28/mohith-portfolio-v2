aaabcfbd831d12a92e2f3b5cc35556ad6efdb6a5
Mohith-Kumar28 <mohithkumar808@gmail.com>
2024-08-12 01:45:17 +0530

    hero padding

--- src/components/hero/BentoHero.tsx
+++ src/components/hero/BentoHero.tsx
@@ -11,17 +11,14 @@ import { LinkPreview } from "../util/link-preview";
 
 export const BentoHero = () => {
   return (
-    <div
-      id="about"
-      className="min-h-screen bg-zinc-900 px-4 py-12 text-zinc-50"
-    >
+    <div id="about" className="min-h-screen bg-zinc-900  py-12 text-zinc-50">
       <motion.div
         initial="initial"
         animate="animate"
         transition={{
           staggerChildren: 0.05,
         }}
-        className="mx-auto grid max-w-4xl grid-flow-dense grid-cols-12 gap-4"
+        className="mx-auto grid  grid-flow-dense grid-cols-12 gap-4"
       >
         <HeaderBlock />
         <SocialsBlock />
